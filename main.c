f64ff8919adecb098346c4cb8a5dff919cc61a62
Kyra Phillips <klphillips@klphillips-dev.naw01.spdev.viasat.io>
2017-09-17 20:02:29 +0000

    Main Update

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 //Name: Kyra Phillips
-//I received help from: Myself, man pages, Holly Radcliffe
+//I received help from: Myself, man pages, Holly Radcliffe, Dr Beaty
 //I provided help to: Slightly helped Holly Radcliffe
 //Resources used: Class notes and the people who helped me
 
@@ -14,34 +14,37 @@
 // Function to create handler
 
 void handler (int signum){
-  write(1,"K",1);
   switch(signum){
-  case 1:
+  case SIGIO:
     signal(SIGIO, handler);
-    write(1,"SIGIO",5);
+    write(1,"SIGIO\n",6);
     break;
-  case 2:
+  case SIGALRM:
     signal(SIGALRM, handler);
-    write(1,"SIGALRM",7);
+    write(1,"SIGALRM\n",8);
     break;
-  case 3:
+  case SIGUSR1:
     signal(SIGUSR1, handler);
-    write(1,"SIGUSR1",7);
+    write(1,"SIGUSR1\n",8);
     break;
   } 
 }
  
 //Main function to be a signal handler to receive signal 
 int main (int argc, char **argv){
-  pid_t pid;
-  pid = fork();
-  pid_t ppid;
-  ppid = getpid();
 
   struct sigaction action;
   action.sa_handler = handler;
   sigemptyset (&action.sa_mask);
   assert(sigaction(SIGUSR1,&action,NULL) ==0);
+  assert(sigaction(SIGIO,&action,NULL)==0);
+  assert(sigaction(SIGALRM,&action,NULL)==0);
+
+  pid_t pid;
+  pid = fork();
+  pid_t ppid;
+  ppid = getpid();
+
   
   if (pid < 0){
     perror("fork");
@@ -59,7 +62,7 @@ int main (int argc, char **argv){
     }
   }
   else {
-    assert;
+    assert(kill(ppid,SIGALRM) ==0);
     assert(kill(ppid, SIGIO) == 0);
     assert(kill(ppid, SIGUSR1) == 0);
   }
